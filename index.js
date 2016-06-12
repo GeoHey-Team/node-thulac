0fdc0ad4f3d4f29c62906129e12ed00588470b06
XuXiang <xux@geohey.com>
2016-06-12 16:27:19 +0800

    简化导出接口

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
 "use strict";
 
-var segmentor = require('./build/Release/segmentor.node')
-
-exports.version = require('./package').version;
-
-exports.segmentor = segmentor;
-//exports.loadModel = segmentor.loadModel;
-//exports.predict = segmentor.predict;
+module.exports = require('./build/Release/segmentor.node')
+module.exports.version = require('./package').version;
 
